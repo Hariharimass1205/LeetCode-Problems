e05799cc179bdf64af8be8e48c5cd887058fa52c
Hariharan <146021544+Hariharimass1205@users.noreply.github.com>
2024-08-13 17:18:11 +0530

    Time: 67 ms (24.70%) | Memory: 56.3 MB (5.18%) - LeetSync

---
+++ 728-self-dividing-numbers/self-dividing-numbers.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+var selfDividingNumbers = function(left, right) {
+   let Container =[]
+let Container2= []
+
+for(let j=left;j<=right;j++){
+Container=[]
+let numberToarrayString = j.toString().split('')
+let arrayToNumber = numberToarrayString.map(str => {
+return Number(str)
+})
+for(let i=0;i<arrayToNumber.length;i++){
+if(j%arrayToNumber[i]===0){
+Container.push(arrayToNumber[i])
+if(Container.length===arrayToNumber.length){
+Container2.push(j)
+}
+}
+}
+}
+return Container2
+};
