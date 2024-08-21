e99d93cccd550121a4fdf317b0d3baad61e5fabe
Hariharan <146021544+Hariharimass1205@users.noreply.github.com>
2024-08-21 09:44:20 +0530

    Time: 49 ms (74.51%) | Memory: 48.3 MB (88.24%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.js
+++ 1448-maximum-69-number/maximum-69-number.js
@@ -3,12 +3,5 @@
  * @return {number}
  */
 var maximum69Number  = function(num) {
-let s = num.toString().split("")
-for(i=0;i<s.length;i++){
-    if(s[i]=='6'){
-        s[i] = '9'
-        break
-    }
-}
-return Number(s.join().replaceAll(",",""))
+ return num.toString().replace('6','9')
 };
