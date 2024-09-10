1b6eb796478517b4a15016e7ea784a26f76ad002
Hariharan <146021544+Hariharimass1205@users.noreply.github.com>
2024-09-10 10:22:32 +0530

    Time: 50 ms (60.09%) | Memory: 48.5 MB (80.96%) - LeetSync

---
+++ 434-number-of-segments-in-a-string/number-of-segments-in-a-string.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSegments = function(s) {
+    s = s.trim(' ') 
+    if(s.length==0) return 0
+    s = s.split(' ')
+    let c = 0
+    let i =0
+    while(i<s.length){
+        if((s[i].trim(' ')).length!==0){
+            c++
+        }
+        i++
+    }
+    return c
+};
